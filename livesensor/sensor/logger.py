8a6b94b16ce1059b5d7512c365dea2a0d5b6deac
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-08-17 14:36:44 +0530

    dataset

---
+++ livesensor/sensor/logger.py
@@ -0,0 +1,16 @@
+import logging 
+import os
+import sys
+from datetime import datetime
+
+Log_File = f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
+logs_path = os.path.join(os.getcwd(), "logs" , Log_File)
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path , Log_File)
+
+logging.basicConfig(
+    filename=  LOG_FILE_PATH,
+    format= "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level= logging.INFO,
+)
--- livesensor/sensor/test.py
+++ livesensor/sensor/test.py
@@ -1,6 +1,19 @@
-from sensor.exception import SensorException
+# testing exception file
+from sensor.exception import SensorException , error_message_detail
+import os
+import sys
+from sensor.logger import logging
 
-try:
-    1/0
-except Exception as e:
-    raise SensorException(e, sys)
+
+def test_exception():
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info(error_message_detail(e,sys))
+        raise SensorException(e ,sys)
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
