2a1228e13db3d0b64c4493c87d699dce214a6959
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-08-19 12:44:24 +0530

    updating readme.md

--- livesensor/main.py
+++ livesensor/main.py
@@ -1,1 +1,19 @@
-help(Exception)
+# testing exceptions
+from sensor.exception import SensorException , error_message_detail
+import os
+import sys
+from sensor.logger import logging
+
+
+def test_exception():
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info(error_message_detail(e,sys))
+        raise SensorException(e ,sys)
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
