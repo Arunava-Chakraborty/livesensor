cdd4e421216ac39275ddb5f92cb073f6e7a9fb81
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-09-02 20:06:24 +0530

    Data validation

--- livesensor/sensor/pipeline/training_pipeline.py
+++ livesensor/sensor/pipeline/training_pipeline.py
@@ -5,6 +5,11 @@ from sensor.logger import logging
 import sys , os 
 from sensor.components.data_ingestion import DataIngestion
 
+from sensor.components.Data_Validation import DataValidation
+from sensor.entity.config_entity import TrainingPipelineConfig , DataIngestionConfig, DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact , DataValidationArtifact
+
+
 
 class TrainPipeline:
 
@@ -29,9 +34,32 @@ class TrainPipeline:
             raise  SensorException(e,sys)
 
 
+    def start_data_validation(self , data_ingestion_artifact:DataIngestionArtifact)->DataValidation:
+    
+        try: 
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config = data_validation_config
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            
+            return data_ingestion_artifact
+        
+        except Exception as e:
+            raise SensorException(e,sys)
 
     def run_pipeline(self):
         try:
-             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+        
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+        
+        
+        
         except Exception as e :    
             raise  SensorException(e,sys)
